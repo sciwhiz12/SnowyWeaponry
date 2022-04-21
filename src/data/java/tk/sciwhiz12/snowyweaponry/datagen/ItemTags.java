0b19ec91a2a29af91d907beb848c9d7c47ece024
sciwhiz12 <arnoldnunag12@gmail.com>
2022-04-21 17:01:53 +0800

    Remove getPath override in ItemTags

--- src/data/java/tk/sciwhiz12/snowyweaponry/datagen/ItemTags.java
+++ src/data/java/tk/sciwhiz12/snowyweaponry/datagen/ItemTags.java
@@ -3,15 +3,12 @@ package tk.sciwhiz12.snowyweaponry.datagen;
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.TagsProvider;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import tk.sciwhiz12.snowyweaponry.Reference;
 import tk.sciwhiz12.snowyweaponry.SnowyWeaponry;
-
-import java.nio.file.Path;
 
 public class ItemTags extends TagsProvider<Item> {
     @SuppressWarnings("deprecation")
@@ -29,11 +26,6 @@ public class ItemTags extends TagsProvider<Item> {
                 .add(Reference.Items.DIAMOND_CHUNK.get());
         tag(Reference.Tags.NUGGETS_NETHERITE)
                 .add(Reference.Items.NETHERITE_NUGGET.get());
-    }
-
-    // Copied from ItemTagsProvider#makePath
-    protected Path getPath(ResourceLocation id) {
-        return this.generator.getOutputFolder().resolve("data/" + id.getNamespace() + "/tags/items/" + id.getPath() + ".json");
     }
 
     public String getName() {
