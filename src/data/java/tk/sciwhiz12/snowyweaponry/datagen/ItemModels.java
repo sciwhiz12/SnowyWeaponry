aa1c795560f0825a38e51f070df62c6c3803bc69
sciwhiz12 <arnoldnunag12@gmail.com>
2023-11-30 05:10:20 +0800

    Cleanup item model generation by using built-in helpers

--- src/data/java/tk/sciwhiz12/snowyweaponry/datagen/ItemModels.java
+++ src/data/java/tk/sciwhiz12/snowyweaponry/datagen/ItemModels.java
@@ -1,58 +1,37 @@
 package tk.sciwhiz12.snowyweaponry.datagen;
 
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
-import net.minecraftforge.client.model.generators.ModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.ForgeRegistries;
 import tk.sciwhiz12.snowyweaponry.Reference.Items;
 import tk.sciwhiz12.snowyweaponry.SnowyWeaponry;
 
-import static java.util.Objects.requireNonNull;
 import static net.minecraft.client.renderer.block.model.ItemModelGenerator.LAYERS;
 
 public class ItemModels extends ItemModelProvider {
-    private final ResourceLocation ITEM_GENERATED = mcLoc("item/generated");
-
     public ItemModels(PackOutput output, ExistingFileHelper existingFileHelper) {
         super(output, SnowyWeaponry.MODID, existingFileHelper);
     }
 
     @Override
     protected void registerModels() {
-        singleTextureItem(Items.DIAMOND_CHUNK.get());
-        singleTextureItem(Items.NETHERITE_NUGGET.get());
+        basicItem(Items.DIAMOND_CHUNK.get());
+        basicItem(Items.NETHERITE_NUGGET.get());
 
-        singleTextureItem(Items.IRON_CORED_SNOWBALL.get());
-        singleTextureItem(Items.GOLD_CORED_SNOWBALL.get());
-        singleTextureItem(Items.DIAMOND_CORED_SNOWBALL.get());
-        singleTextureItem(Items.NETHERITE_CORED_SNOWBALL.get());
-        singleTextureItem(Items.EXPLOSIVE_SNOWBALL.get());
+        basicItem(Items.IRON_CORED_SNOWBALL.get());
+        basicItem(Items.GOLD_CORED_SNOWBALL.get());
+        basicItem(Items.DIAMOND_CORED_SNOWBALL.get());
+        basicItem(Items.NETHERITE_CORED_SNOWBALL.get());
+        basicItem(Items.EXPLOSIVE_SNOWBALL.get());
 
-        singleTextureItem(Items.WAFER_CONE.get());
-        singleTextureItem(Items.SNOW_CONE.get());
-        singleTextureItem(Items.GOLDEN_SNOW_CONE.get());
+        basicItem(Items.WAFER_CONE.get());
+        basicItem(Items.SNOW_CONE.get());
+        basicItem(Items.GOLDEN_SNOW_CONE.get());
 
         getBuilder(Items.POTION_SNOW_CONE.getId().toString())
                 .parent(new ModelFile.UncheckedModelFile("item/generated"))
                 .texture(LAYERS.get(0), modLoc(ITEM_FOLDER + '/' + "potion_snow_cone_base"))
                 .texture(LAYERS.get(1), modLoc(ITEM_FOLDER + '/' + "potion_snow_cone_overlay"));
-    }
-
-    void singleTextureItem(Item item) {
-        final ResourceLocation itemName = name(item);
-        singleTexture(
-                itemName.getPath(),
-                ITEM_GENERATED,
-                LAYERS.get(0),
-                modLoc(ModelProvider.ITEM_FOLDER + "/" + itemName.getPath())
-        );
-    }
-
-    private static ResourceLocation name(Item item) {
-        return requireNonNull(ForgeRegistries.ITEMS.getKey(item));
     }
 }
