f7c2ac0513cda7db9710c559b508fbe549b38a8f
sciwhiz12 <arnoldnunag12@gmail.com>
2024-09-02 01:36:23 +0800

    Update to JEI 19 and remove use of deprecated type

--- src/main/java/dev/sciwhiz12/snowyweaponry/jei/PotionSubtypeInterpreter.java
+++ src/main/java/dev/sciwhiz12/snowyweaponry/jei/PotionSubtypeInterpreter.java
@@ -1,33 +1,34 @@
 package dev.sciwhiz12.snowyweaponry.jei;
 
-import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
+import mezz.jei.api.ingredients.subtypes.ISubtypeInterpreter;
 import mezz.jei.api.ingredients.subtypes.UidContext;
 import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.PotionContents;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-public class PotionSubtypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack> {
+public class PotionSubtypeInterpreter implements ISubtypeInterpreter<ItemStack> {
     public static final PotionSubtypeInterpreter INSTANCE = new PotionSubtypeInterpreter();
 
     private PotionSubtypeInterpreter() {
     }
 
     @Override
-    public String apply(ItemStack stack, UidContext context) {
-        final @Nullable PotionContents contents = stack.get(DataComponents.POTION_CONTENTS);
-        if (contents == null) {
-            return NONE;
-        }
+    public @Nullable Object getSubtypeData(ItemStack ingredient, UidContext context) {
+        final @Nullable PotionContents contents = ingredient.get(DataComponents.POTION_CONTENTS);
+        if (contents == null) return null;
 
-        final StringBuilder stringBuilder = new StringBuilder(contents.potion().map(Holder::getRegisteredName).orElse(""));
+        return contents.potion().orElse(null);
+    }
 
-        for (MobEffectInstance effect : contents.customEffects()) {
-            stringBuilder.append(";").append(effect);
-        }
+    @Override
+    public String getLegacyStringSubtypeInfo(ItemStack ingredient, UidContext context) {
+        if (ingredient.getComponentsPatch().isEmpty()) return "";
 
-        return stringBuilder.toString();
+        PotionContents contents = ingredient.getOrDefault(DataComponents.POTION_CONTENTS, PotionContents.EMPTY);
+        String itemDescriptionId = ingredient.getItem().getDescriptionId();
+        String potionEffectId = contents.potion().map(Holder::getRegisteredName).orElse("none");
+        return itemDescriptionId + ".effect_id." + potionEffectId;
     }
 }
